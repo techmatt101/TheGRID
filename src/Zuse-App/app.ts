34fa0584492a7549edaeee96ef68a52ef58bfa59
techmatt101 <matt@kempmail.co.uk>
2015-02-14 08:09:33 +0000

    basic setup of Zuse app with hapi framework Also link to docs from the api and error info

---
+++ src/Zuse-App/app.ts
@@ -0,0 +1,47 @@
+/// <reference path="dt/node.d.ts" />
+/// <reference path="dt/config.d.ts" />
+/// <reference path="dt/hapi.d.ts" />
+/// <reference path="dt/joi.d.ts" />
+/// <reference path="dt/boom.d.ts" />
+
+import config = require('config');
+import Hapi = require('hapi');
+import Joi = require('joi');
+import Boom = require('boom');
+
+var server = new Hapi.Server();
+
+server.connection({
+    port: config.get('Server.port'),
+    host: config.get('Server.host')
+});
+
+server.register({
+    register: require('good'),
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args: [{log: '*', response: '*'}]
+        }]
+    }
+}, (err) => {
+    if (err) console.error('Failed to load a plugin:', err);
+});
+
+// 404
+server.route({
+    method: '*',
+    path: '/{p*}',
+    handler: (request, reply) => reply(Boom.notFound("Resource not found. Please visit documentation: http://docs.thegrid.apiary.io/"))
+});
+
+//Docs
+server.route({
+    method: '*',
+    path: '/docs',
+    handler: (request, reply) => reply.redirect('http://docs.thegrid.apiary.io/')
+});
+
+server.start(() => {
+    console.log('Server running at: ' + server.info.uri);
+});
