cba8b37b7e31547ef9794d003d9412104b9f347b
techmatt101 <matt@kempmail.co.uk>
2015-04-08 18:14:30 +0100

    small hack to remove 'index/' in module path A better solution to this would be nice

--- src/Services-Lib/gulpfile.js
+++ src/Services-Lib/gulpfile.js
@@ -1,18 +1,23 @@
 var gulp = require('gulp');
+var replace = require('gulp-replace');
 var shell = require('gulp-shell');
 var dts = require('dts-bundle');
 
 gulp.task('default', ['services']);
 
-gulp.task('build', shell.task('tsc index.ts --outDir build/ --target ES5 --module commonjs --declaration'));
+gulp.task('build', shell.task('tsc index.ts --outDir bin/ --target ES5 --module commonjs --declaration'));
 
 gulp.task('services', ['build'], function() {
     dts.bundle({
         name: 'services',
-        main: 'build/index.d.ts',
+        main: 'bin/index.d.ts',
         prefix: '',
         externals: true
     });
+
+    gulp.src('bin/services.d.ts')
+        .pipe(replace('/index//', '/'))
+        .pipe(gulp.dest('build/'));
 });
 
 gulp.task('update-typings', ['master-control']);
@@ -25,7 +30,7 @@ function buildAppTypings(appName, appPath) {
     gulp.task(appName, [appName + '-shell'], function() {
         dts.bundle({
             name: appName,
-            main: appPath + 'build/app.d.ts',
+            main: appPath + 'bin/app.d.ts',
             out: '../Services-Lib/typings/' + appName + '.d.ts',
             prefix: '',
             externals: true
