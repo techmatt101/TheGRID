f707e5e37d747aeaae26eb7ae1086a06bfa724a9
techmatt101 <matt@kempmail.co.uk>
2015-04-25 05:09:39 +0100

    fixed memory leak

--- src/MasterControl-App/services/AuthTokenService.ts
+++ src/MasterControl-App/services/AuthTokenService.ts
@@ -2,7 +2,7 @@ import uid = require('uid');
 
 
 module AuthTokenService {
-    var tokenTable = {};
+    var tokens = [];
 
     export interface IToken<T> {
         created: number
@@ -12,25 +12,28 @@ module AuthTokenService {
     // Clean up
     setInterval(() => {
         var now = Date.now();
-        for (var key in tokenTable) {
-            if (tokenTable[key].created < now) {
-                tokenTable[key] = undefined;
+        var activeTokens = [];
+        for(var token of tokens) {
+            if (token.created > now) {
+                activeTokens.push(token);
             }
         }
+        tokens = activeTokens;
     }, 1000 * 60 * 60); // 1 hour
 
 
     export function generateToken (data = null, timespan = 1000 * 60 * 5, tokenSize = 16) {
         var token = uid(tokenSize);
-        tokenTable[token] = {
+        tokens.push({
+            key: token,
             created: Date.now() + timespan,
             data: data
-        };
+        });
         return token;
     }
 
     export function getToken<T> (tokenKey) : IToken<T> {
-        var token = tokenTable[tokenKey];
+        var token = tokens.find((x) => x.key === tokenKey);
         if(typeof token === 'undefined') return null;
         if(token.created < Date.now()) return null;
         return token;
